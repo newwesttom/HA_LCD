75238a65f55b60222d56fd3a805d5e456e83431e
newwesttom <newwesttom@gmail.com>
2025-07-19 19:48:58 +0200

    Create HA_CONFIG.py

---
+++ HA_CONFIG.py
@@ -0,0 +1,21 @@
+BASE_URL = "http://homeassistant.local:8123"
+TOKEN = "long life token,  get it from user profile in Home Assistant"
+# I created the following helpers with text type and then created automations to update them.
+
+labels = [
+    "input_text.picotext_label1",
+    "input_text.picotext_label2",
+    "input_text.picotext_label3",
+    "input_text.picotext_label4",
+    "input_text.picotext_label5",
+    "input_text.picotext_label6",
+]
+
+states = [
+    "input_text.picotext_value1",
+    "input_text.picotext_value2",
+    "input_text.picotext_value3",
+    "input_text.picotext_value4",
+    "input_text.picotext_value5",
+    "input_text.picotext_value6",
+]
